0df2ca9d98a7abf89cb788b2eab3ddbc112c5311
My Name <parisliahyun@gmail.com>
2013-11-25 21:21:59 -0500

    committing

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'sass-rails', '~> 4.0.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'pg'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem "multi_json", "~> 1.7.7"
+gem 'pry-rails', group: [:development, :test]
+gem 'bcrypt-ruby', '~> 3.0.0'
+gem "citibikenyc", "~> 0.0.1"
+gem "geocoder", "~> 1.1.8"
+gem 'rails_12factor', group: :production
+gem 'bootstrap-sass', github: 'thomas-mcdonald/bootstrap-sass', branch: '3'
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Citibike::Application.config.secret_key_base = 'ba8de997ab885cf1cf1185700604cd06bbe0fb57c4d46e17bb996b97acf02ee05f7c8b0ce5bf023898da0bf31faabb4e2a8275a4ba24ecbb1900f451a15366f9'
