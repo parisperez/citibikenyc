91c63c24b792df99d4aea7299ea1fd648bddaf0c
My Name <parisliahyun@gmail.com>
2013-11-30 11:08:52 -0800

    working code on exchange form

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -7,35 +7,23 @@ class ExchangesController < ApplicationController
   end
 
   def create
-    # ADDRESS
-    # @coordinates = Geocoder.coordinates(params[:address])
-    # unless @coordinates.nil?
-
-    #   @coordinates = { latitude: @coordinates[0], longitude: @coordinates[1] }
-    #   @all_citibike_stations = Citibikenyc.stations.values[2]
-
-    #   @winning_station = @all_citibike_stations.min_by do |station|
-    #     distance_x = @coordinates[:longitude] - station["longitude"]
-    #     distance_y = @coordinates[:latitude] - station["latitude"]
-    #     Math.hypot( distance_x, distance_y )
-    #   end 
-    # end  
-
-      # EXCHANGE FORM
-      @exchange = Exchange.new(exchange_params)
-      binding.pry
+      @exchanges = Exchange.all
+      @exchange = Exchange.new(exchange_params)  
+      choice = params[:is_bike]    
+      if choice = true
+        @exchange.is_bike = true
+      else
+        @exchange.is_bike = false
+      end
+       # binding.pry
       if @exchange.save
-
+     
         render :results
       else
         render :new  
       end
     
   end
-
-    # def index
-    #   redirect_to new_exchange_path
-    # end
 
   private
 
