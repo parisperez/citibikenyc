e2c175abdde4c6668945bf9134505d9127f7967e
My Name <parisliahyun@gmail.com>
2013-12-03 07:48:57 -0500

    committing

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -19,15 +19,15 @@ class Exchange < ActiveRecord::Base
   #   return "#{Date::MONTHNAMES[d.day]} #{d.dhr}, #{d.dmin}"
   # end
 
-  def set_vendor(exchange, id)
-    self.vendor_id = current_user.id
-  end
+  # def set_vendor(exchange, id)
+  #   self.vendor_id = current_user.id
+  # end
 
-  def set_requester(exchange, id)
-    @exchange = Exchange.find_by_id(params[:id])
-    @exchange.requester_id = current_user.id
-    @exchange.save!
-  end
+  # def set_requester(exchange, id)
+  #   @exchange = Exchange.find_by_id(params[:id])
+  #   @exchange.requester_id = current_user.id
+  #   @exchange.save!
+  # end
 
 end
 
