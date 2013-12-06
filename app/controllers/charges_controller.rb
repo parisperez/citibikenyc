73aa71164609f2fca019a5b05604332d448a53c4
My Name <parisliahyun@gmail.com>
2013-12-05 23:05:43 -0500

    uncomment stripe:

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,27 +1,27 @@
-# class ChargesController < ApplicationController
+class ChargesController < ApplicationController
 
-#   def new
-#   end
+  def new
+  end
 
-#   def create
-#     # Amount in cents
-#     @amount = 500
+  def create
+    # Amount in cents
+    @amount = 500
 
-#     customer = Stripe::Customer.create(
-#       :email => 'example@stripe.com',
-#       :card  => params[:stripeToken]
-#     )
+    customer = Stripe::Customer.create(
+      :email => 'example@stripe.com',
+      :card  => params[:stripeToken]
+    )
 
-#     charge = Stripe::Charge.create(
-#       :customer    => customer.id,
-#       :amount      => @amount,
-#       :description => 'Rails Stripe customer',
-#       :currency    => 'usd'
-#     )
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
 
-#   rescue Stripe::CardError => e
-#     flash[:error] = e.message
-#     redirect_to charges_path
-#   end
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path
+  end
 
-# end
+end
