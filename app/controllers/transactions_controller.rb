0a3e89546759edb806c0dd59c359190c2e07dc49
My Name <parisliahyun@gmail.com>
2014-02-05 14:41:51 -0800

    changed amount to fee_amount

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -26,7 +26,7 @@ class TransactionsController < ApplicationController
       id: params[:id]
       )
     sale = @exchange.sales.create(
-      amount: @exchange.price,
+      fee_amount: @exchange.price,
       email: params[:email],
       stripe_token: params[:stripeToken]
       )
