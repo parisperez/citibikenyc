4ce60a500fd7be570168a45c98fb0556973b050a
My Name <parisliahyun@gmail.com>
2014-02-04 21:00:18 -0800

    stripe keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,4 +99,11 @@ Citibike::Application.configure do
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+Rails.configuration.stripe = {
+publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+secret_key: ENV['STRIPE_SECRET_KEY'],
+}
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
 end
