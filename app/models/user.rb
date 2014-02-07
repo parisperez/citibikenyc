a1577665633515e4913db7f22557c500f1ac78b4
My Name <parisliahyun@gmail.com>
2014-02-07 14:11:38 -0800

    confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-       :recoverable, :rememberable, :trackable, :validatable
+       :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :email, presence: true, uniqueness: true
   letsrate_rateable
   letsrate_rater
