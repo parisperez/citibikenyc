6be9b9a782ddcc96304f2936dc16cd959992957c
My Name <parisliahyun@gmail.com>
2014-02-11 20:55:15 -0800

    to_f

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -30,7 +30,7 @@ class TransactionsController < ApplicationController
       stripe_customer_id = exchange_user.stripe_customer_id 
     # sale record
     sale = @exchange.sales.create(
-      amount: @exchange.price,
+      amount: @exchange.price.to_f,
       email: params[:email],
       stripe_token: params[:stripeToken],
       vendor_id: @exchange.vendor_id,
