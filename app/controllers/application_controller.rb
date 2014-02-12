b8ed46f5b53e4345eb2275804edbf0462fd40501
My Name <parisliahyun@gmail.com>
2014-02-12 11:42:19 -0800

    user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:username]
-    devise_parameter_sanitizer.for(:account_update) << [:username, :role, :stripe_recipient_id] 
+    devise_parameter_sanitizer.for(:account_update) << [:username, :role, :stripe_recipient_id, :phone_number, :image_url, :headline] 
   end
 
   def after_sign_in_path_for(resource)
