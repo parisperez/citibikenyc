efd6943995b9f390d4823f5c8aceadfa4bb23b76
My Name <parisliahyun@gmail.com>
2014-02-12 15:32:31 -0800

    changed smtp address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -126,8 +126,8 @@ Citibike::Application.configure do
   # }
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
+  address: "oxmail.registrar-servers.com",
+  port: 465,
   domain: ENV["GMAIL_DOMAIN"],
   authentication: "plain",
   enable_starttls_auto: true,
