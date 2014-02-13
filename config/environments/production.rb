7831721188a22e02b763d83e77c77c844f4b6c1a
My Name <parisliahyun@gmail.com>
2014-02-12 21:50:49 -0800

    changed back to gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,24 +115,24 @@ Citibike::Application.configure do
 
   config.action_mailer.delivery_method = :smtp 
 
-  # config.action_mailer.smtp_settings = {
-  # address: "smtp.gmail.com",
-  # port: 587,
-  # domain: ENV["GMAIL_DOMAIN"],
-  # authentication: "plain",
-  # enable_starttls_auto: true,
-  # user_name: ENV["GMAIL_USERNAME"],
-  # password: ENV["GMAIL_PASSWORD"]
-  # }
-
   config.action_mailer.smtp_settings = {
-  address: "oxmail.registrar-servers.com",
-  port: 25,
+  address: "smtp.gmail.com",
+  port: 587,
   domain: ENV["GMAIL_DOMAIN"],
-  authentication: "Normal Password",
+  authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
   password: ENV["GMAIL_PASSWORD"]
   }
+
+  # config.action_mailer.smtp_settings = {
+  # address: "oxmail.registrar-servers.com",
+  # port: 25,
+  # domain: ENV["GMAIL_DOMAIN"],
+  # authentication: "Normal Password",
+  # enable_starttls_auto: true,
+  # user_name: ENV["GMAIL_USERNAME"],
+  # password: ENV["GMAIL_PASSWORD"]
+  # }
 
 end
