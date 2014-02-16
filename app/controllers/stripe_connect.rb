c08278e5d3e2e4c18b877dc62d477aae81e3cb3a
My Name <parisliahyun@gmail.com>
2014-02-15 17:05:21 -0800

    tweak

--- app/controllers/stripe_connect.rb
+++ app/controllers/stripe_connect.rb
@@ -4,8 +4,12 @@ class StripeConnectController < ApplicationController
     current_user.stripe_id = auth_hash['uid']
     current_user.stripe_access_key = auth_hash['credentials']['token']
     current_user.stripe_publishable_key = auth_hash['info']['stripe_publishable_key']
-    current_user.save!
-    flash[:notice] = "Stripe info saved"
-    redirect_to user_path(current_user)
+    if current_user.save
+      flash[:notice] = "Stripe info saved"
+      redirect_to user_path(current_user)
+    else
+      redirect_to user_path(current_user)
+      flash[:notice] = "Something went wrong."
+    end
   end
 end
