fea2157f3a7d3b9689617b6b3965f80ca6ac12fe
My Name <parisliahyun@gmail.com>
2014-02-17 14:52:06 -0800

    comment all create

--- app/controllers/stripe_connect.rb
+++ app/controllers/stripe_connect.rb
@@ -1,9 +1,9 @@
 class StripeConnectController < ApplicationController
 
   def create
-    auth_hash = params[:auth_hash]
+    # auth_hash = params[:auth_hash]
     # current_user.stripe_id = auth_hash['uid']
-    current_user.stripe_access_key = auth_hash['access_token']
+    # current_user.stripe_access_key = auth_hash['credentials']['token']
     # current_user.stripe_publishable_key = auth_hash['info']['stripe_publishable_key']
     raise request.env["omniauth.auth"].to_yaml
     if current_user.save
