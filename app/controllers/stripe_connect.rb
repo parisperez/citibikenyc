15022623258937dd1bc3f8958f8fe6bb4de29db1
My Name <parisliahyun@gmail.com>
2014-02-17 15:11:10 -0800

    uncomment create

--- app/controllers/stripe_connect.rb
+++ app/controllers/stripe_connect.rb
@@ -3,16 +3,16 @@ class StripeConnectController < ApplicationController
   def create
     auth_hash = request.env["omniauth.auth"]
     # auth_hash = params[:auth_hash]
-    # current_user.stripe_id = auth_hash['uid']
-    # current_user.stripe_access_key = auth_hash['credentials']['token']
-    # current_user.stripe_publishable_key = auth_hash['info']['stripe_publishable_key']
-    # if current_user.save
-    #   flash[:notice] = "Stripe info saved"
-    #   redirect_to root_path
-    # else
-    #   redirect_to root_path
-    #   flash[:notice] = "Something went wrong."
-    # end
+    current_user.stripe_id = auth_hash['uid']
+    current_user.stripe_access_key = auth_hash['credentials']['token']
+    current_user.stripe_publishable_key = auth_hash['info']['stripe_publishable_key']
+    if current_user.save
+      flash[:notice] = "Stripe info saved"
+      redirect_to root_path
+    else
+      redirect_to root_path
+      flash[:notice] = "Something went wrong."
+    end
     render :new
   end
 
