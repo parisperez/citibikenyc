6f209d0ba1aebc63071e943ee65c269240f91d47
My Name <parisliahyun@gmail.com>
2014-02-17 14:26:45 -0800

    comment out from create

--- app/controllers/stripe_connect.rb
+++ app/controllers/stripe_connect.rb
@@ -2,9 +2,9 @@ class StripeConnectController < ApplicationController
 
   def create
     auth_hash = params[:auth_hash]
-    current_user.stripe_id = auth_hash['uid']
+    # current_user.stripe_id = auth_hash['uid']
     current_user.stripe_access_key = auth_hash['credentials']['token']
-    current_user.stripe_publishable_key = auth_hash['info']['stripe_publishable_key']
+    # current_user.stripe_publishable_key = auth_hash['info']['stripe_publishable_key']
     if current_user.save
       flash[:notice] = "Stripe info saved"
       redirect_to root_path
@@ -12,7 +12,7 @@ class StripeConnectController < ApplicationController
       redirect_to root_path
       flash[:notice] = "Something went wrong."
     end
-    # render :new
+    render :new
   end
 
 end
