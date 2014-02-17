5046c51e7fd8104b9b98718c3e7578acbde72469
My Name <parisliahyun@gmail.com>
2014-02-17 10:37:44 -0800

    trying auth path version

--- app/controllers/stripe_connect.rb
+++ app/controllers/stripe_connect.rb
@@ -1,18 +1,18 @@
-class StripeConnectController < ApplicationController
+# class StripeConnectController < ApplicationController
 
-  def create
-    auth_hash = params[:auth_hash]
-    current_user.stripe_id = auth_hash['uid']
-    current_user.stripe_access_key = auth_hash['credentials']['token']
-    current_user.stripe_publishable_key = auth_hash['info']['stripe_publishable_key']
-    if current_user.save
-      flash[:notice] = "Stripe info saved"
-      redirect_to root_path
-    else
-      redirect_to root_path
-      flash[:notice] = "Something went wrong."
-    end
-    render :new
-  end
+#   def create
+#     auth_hash = params[:auth_hash]
+#     current_user.stripe_id = auth_hash['uid']
+#     current_user.stripe_access_key = auth_hash['credentials']['token']
+#     current_user.stripe_publishable_key = auth_hash['info']['stripe_publishable_key']
+#     if current_user.save
+#       flash[:notice] = "Stripe info saved"
+#       redirect_to root_path
+#     else
+#       redirect_to root_path
+#       flash[:notice] = "Something went wrong."
+#     end
+#     render :new
+#   end
 
-end
+# end
