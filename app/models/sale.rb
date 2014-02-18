6a9078854386db6f31bc26903caa4f0484c7c0c6
My Name <parisliahyun@gmail.com>
2014-02-17 21:07:26 -0800

    same

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -38,6 +38,7 @@ class Sale < ActiveRecord::Base
       charge = Stripe::Charge.create(
       {
         :amount   => self.amount,
+        :customer => self.customer_id,
         :currency => "usd",
         :card => stripe_token,
         :application_fee => self.sendangel_fee,
