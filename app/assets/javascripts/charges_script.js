4fdb7ab9bbdd4099b8db69847761aca565441c8b
My Name <parisliahyun@gmail.com>
2014-02-19 14:04:18 -0800

    edit style

--- app/assets/javascripts/charges_script.js
+++ app/assets/javascripts/charges_script.js
@@ -1,4 +1,4 @@
-$(function(){
-Stripe.setPublishableKey('pk_test_78JfzKOc6b2AHF9PVbHe37aK');
-// Stripe.setPublishableKey('<%= Rails.configuration.stripe[:publishable_key] %>');
-}); 
+// $(function(){
+// Stripe.setPublishableKey('pk_test_78JfzKOc6b2AHF9PVbHe37aK');
+// // Stripe.setPublishableKey('<%= Rails.configuration.stripe[:publishable_key] %>');
+// }); 
